4ab13fe604bd8b21d81a3d558210563e96ee9279
jineksh <chovatiyajinex@gmail.com>
2026-05-14 15:07:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const getBtn = document.querySelector("#get-otp");
+const input = document.querySelector("#otp-input");
+const verifyBtn = document.querySelector("#verify-btn");
+const message = document.querySelector("#otp-message");
+
+function getOtp(){
+
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    localStorage.setItem("otp", otp);
+
+    input.value = otp;
+
+}
+
+
+function validateOtp(){
+    const userInput = input.value;
+    const storedOtp = localStorage.getItem("otp");
+    if(userInput === storedOtp){
+        message.textContent = "OTP is valid!";
+    } else {
+        message.textContent = "OTP is invalid!";
+        message.style.color = "red";
+    }
+
+    setTimeout(()=>{
+        input.value = "";
+        message.textContent = "";
+        
+    },3000);
+
+    localStorage.removeItem("otp");
+
+}
+
+getBtn.addEventListener('click', getOtp);
+verifyBtn.addEventListener('click', validateOtp);
